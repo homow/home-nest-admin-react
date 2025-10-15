36fb08dd84773f9e4a42df058e737e4cd98899c0
homow <homowsh897@gmail.com>
2025-10-15 22:12:40 +0330

    edit fonts name

--- src/layout/SideBar/index.jsx
+++ src/layout/SideBar/index.jsx
@@ -6,7 +6,7 @@ export default function SideBar({setMobileNavOpen, mobileNavOpen}) {
     const [collapsed, setCollapsed] = useState(false)
 
     return (
-        <aside id="side-bar" className={`fixed overflow-hidden top-0 pr-4.5 transition-all text-sm max-md:mobile-nav ${mobileNavOpen ? "max-md:left-0" : "max-md:-left-58"} md:w-custom md:text-base}`}>
+        <aside id="side-bar" className={`fixed overflow-hidden top-0 pr-4.5 transition-all text-sm max-md:mobile-nav ${mobileNavOpen ? "max-md:right-0" : "max-md:-right-58"} md:w-custom md:text-base}`}>
             <SideBarHeader collapsed={collapsed} setCollapsed={setCollapsed}/>
             <SideBarMenu setMobileNavOpen={setMobileNavOpen} collapsed={collapsed}/>
         </aside>
