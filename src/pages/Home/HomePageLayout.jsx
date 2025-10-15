6617820758684177cd150f5f52d8445b3407b2cd
homow <homowsh897@gmail.com>
2025-10-15 22:09:42 +0330

    config for RTL

--- src/pages/Home/HomePageLayout.jsx
+++ src/pages/Home/HomePageLayout.jsx
@@ -1,9 +1,6 @@
-import TopSection from "./TopSection/TopSection.jsx";
-
 export default function HomePageLayout() {
     return (
         <div className={"pt-12"}>
-            <TopSection/>
         </div>
     )
 }
