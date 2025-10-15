02587fc811ba086614cff774283c585cc71ed70b
homow <homowsh897@gmail.com>
2025-10-15 22:07:10 +0330

    config BASE_PATH

---
+++ src/config.js
@@ -0,0 +1,2 @@
+const BASE_PATH = import.meta.env.VITE_BASE_PATH;
+export default BASE_PATH;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React, {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
+import BASE_PATH from "@/config.js";
 import "@/styles/global.css"
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={BASE_PATH}>
             <App/>
         </BrowserRouter>
     </StrictMode>,
