c726073b27429c83921ac48b424950fae2c95ff3
homow <homowsh897@gmail.com>
2025-10-18 01:04:39 +0330

    config supabase files

--- api/spabaseServer.js
+++ api/spabaseServer.js
@@ -1,13 +1,8 @@
 import {createClient} from "@supabase/supabase-js";
 
-const supabaseAnon = createClient(
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_ANON_KEY
-)
-
 const supabaseAdmin = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_SERVICE_ROLE_KEY
 )
 
-export {supabaseAnon, supabaseAdmin};
+export default supabaseAdmin;
