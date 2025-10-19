f9bd667d0aa78436d51ca99b138a11bddcd9a77b
homow <homowsh897@gmail.com>
2025-10-19 18:02:52 +0330

    config error handling

---
+++ src/components/ui/AlertModal.jsx
@@ -0,0 +1,32 @@
+import {useEffect, useState} from "react";
+
+export default function AlertModal({message, isOpen, type = "error"}) {
+    const [open, setOpen] = useState(false);
+
+    const bgHeader = type === "error" ? "text-red-500" : "text-green-500";
+    const btnBg = type === "error" ? "bg-red-500 hover:bg-red-600" : "bg-green-500 hover:bg-green-600";
+    const title = type === "error" ? "خطا" : "موفقیت";
+
+    useEffect(() => {
+        setOpen(isOpen);
+    }, [isOpen]);
+
+    return (
+        <div className={`fixed top-10 left-0 right-0 flex items-center justify-center z-30 ${open ? "block" : "hidden"}`}>
+            <div className="bg-white dark:bg-gray-800 rounded-xl shadow-xl max-w-sm w-full p-6 space-y-4">
+                <h3 className={`text-lg font-semibold ${bgHeader}`}>
+                    {title}
+                </h3>
+                <p className="text-gray-700 dark:text-gray-300">{message}</p>
+                <div className="flex justify-end">
+                    <button
+                        onClick={() => setOpen(false)}
+                        className={`cursor-pointer px-4 py-2 rounded-lg text-white ${btnBg} transition`}
+                    >
+                        بستن
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,13 +1,15 @@
 import {useEffect, useState} from "react"
 import {login} from "@api/callApi.js";
 import useIsLogin from "@hooks/useIsLogin.jsx"
+import AlertModal from "@components/ui/AlertModal.jsx";
 
 export default function Login() {
     const [showPassword, setShowPassword] = useState(false);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [remember, setRemember] = useState(false);
-    const {setUserIsLogin} = useIsLogin();
+    const [alertModal, setAlertModal] = useState({isOpen: true, type: "error", message: ""});
+    // const {setUserIsLogin} = useIsLogin();
 
     useEffect(() => {
         document.title = "ورود به اکانت ادمین | آشیانه";
@@ -24,77 +26,83 @@ export default function Login() {
 
         try {
             const res = await login(userInfo)
-            setUserIsLogin(true)
+            // setUserIsLogin(true)
             console.log(res)
         } catch (e) {
+            console.log(typeof e);
             console.log(e)
         }
     }
 
     return (
-        <section className="flex items-center justify-center min-h-screen">
-            <div className="w-full max-w-md bg-white/10 rounded-2xl shadow-lg p-8 space-y-6">
-                <h2 className="text-2xl font-bold text-center">خوش آومدی</h2>
-                <p className="text-sm text-center text-secondary-txt">
-                    لطفا به اکانت ادمین وارد شو.
-                </p>
+        <>
+            <AlertModal {...alertModal}/>
+            <section className="flex items-center justify-center min-h-screen">
+                <div className="w-full max-w-md bg-white/10 rounded-2xl shadow-lg p-8 space-y-6">
+                    <h2 className="text-2xl font-bold text-center">خوش آومدی</h2>
+                    <p className="text-sm text-center text-secondary-txt">
+                        لطفا به اکانت ادمین وارد شو.
+                    </p>
 
-                <form className="space-y-6" onSubmit={loginHandler}>
-                    <div>
-                        <label htmlFor="email" className="block text-sm font-medium">ایمیل</label>
-                        <input
-                            dir={"ltr"}
-                            value={email}
-                            onChange={event => setEmail(event.target.value)}
-                            name="email"
-                            type="email"
-                            id="email"
-                            placeholder="you@example.com"
-                            className="mt-1 block w-full rounded-lg border border-gray-300 px-4 py-2 placeholder-gray-400 focus:border-violet-500 focus:ring-1 focus:ring-violet-500 outline-none transition"
-                        />
-                    </div>
-
-                    <div>
-                        <label htmlFor="password" className="block text-sm font-medium">پسورد</label>
-                        <div className="relative">
+                    <form className="space-y-6" onSubmit={loginHandler}>
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium">ایمیل</label>
                             <input
+                                required
                                 dir={"ltr"}
-                                value={password}
-                                onChange={event => setPassword(event.target.value)}
-                                name="password"
-                                type={showPassword ? "text" : "password"}
-                                id="password"
-                                placeholder="*******"
-                                className="mt-1 block w-full rounded-lg border border-gray-300 px-4 py-2 pr-10 placeholder-gray-400 focus:border-violet-500 focus:ring-1 focus:ring-violet-500 outline-none transition"
+                                value={email}
+                                onChange={event => setEmail(event.target.value)}
+                                name="email"
+                                type="email"
+                                id="email"
+                                placeholder="you@example.com"
+                                className="mt-1 block w-full rounded-lg border border-gray-300 px-4 py-2 placeholder-gray-400 focus:border-violet-500 focus:ring-1 focus:ring-violet-500 outline-none transition"
                             />
-                            <button
-                                type="button"
-                                onClick={() => setShowPassword(!showPassword)}
-                                className="absolute right-2 top-2 text-sm text-gray-500 hover:text-violet-500 cursor-pointer"
-                            >
-                                {showPassword ? "مخفی" : "نمایش"}
-                            </button>
                         </div>
-                    </div>
 
-                    <div className="flex items-center justify-between text-sm text-gray-400">
-                        <label className="flex items-center gap-2 cursor-pointer">
-                            <input
-                                onChange={event => setRemember(event.target.checked)}
-                                type="checkbox"
-                                className="w-4 h-4 rounded border-2 border-gray-400 appearance-none checked:bg-violet-500 checked:border-violet-600 cursor-pointer"/>
-                            <span>منو یادت باشه</span>
-                        </label>
-                    </div>
+                        <div>
+                            <label htmlFor="password" className="block text-sm font-medium">پسورد</label>
+                            <div className="relative">
+                                <input
+                                    required
+                                    dir={"ltr"}
+                                    value={password}
+                                    onChange={event => setPassword(event.target.value)}
+                                    name="password"
+                                    type={showPassword ? "text" : "password"}
+                                    id="password"
+                                    placeholder="*******"
+                                    className="mt-1 block w-full rounded-lg border border-gray-300 px-4 py-2 pr-10 placeholder-gray-400 focus:border-violet-500 focus:ring-1 focus:ring-violet-500 outline-none transition"
+                                />
+                                <button
+                                    type="button"
+                                    onClick={() => setShowPassword(!showPassword)}
+                                    className="absolute right-2 top-2 text-sm text-gray-500 hover:text-violet-500 cursor-pointer"
+                                >
+                                    {showPassword ? "مخفی" : "نمایش"}
+                                </button>
+                            </div>
+                        </div>
 
-                    <button
-                        type="submit"
-                        className="w-full cursor-pointer bg-violet-500 text-white font-semibold rounded-lg py-2 hover:bg-violet-600 transition"
-                    >
-                        Login
-                    </button>
-                </form>
-            </div>
-        </section>
+                        <div className="flex items-center justify-between text-sm text-gray-400">
+                            <label className="flex items-center gap-2 cursor-pointer">
+                                <input
+                                    onChange={event => setRemember(event.target.checked)}
+                                    type="checkbox"
+                                    className="w-4 h-4 rounded border-2 border-gray-400 appearance-none checked:bg-violet-500 checked:border-violet-600 cursor-pointer"/>
+                                <span>منو یادت باشه</span>
+                            </label>
+                        </div>
+
+                        <button
+                            type="submit"
+                            className="w-full cursor-pointer bg-violet-500 text-white font-semibold rounded-lg py-2 hover:bg-violet-600 transition"
+                        >
+                            Login
+                        </button>
+                    </form>
+                </div>
+            </section>
+        </>
     )
 }
