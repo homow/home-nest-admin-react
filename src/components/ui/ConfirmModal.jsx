1d471374ceebb473773310961ba73f647cdcfda1
homow <homowsh897@gmail.com>
2025-10-22 21:15:46 +0330

    config logoutHandler in AccountMenu

---
+++ src/components/ui/ConfirmModal.jsx
@@ -0,0 +1,52 @@
+import {useEffect, useState} from "react";
+
+export default function ConfirmModal({isOpen, message, title = "تأیید", onConfirm, onCancel, dangerMode = false}) {
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        setOpen(isOpen);
+    }, [isOpen]);
+
+    const confirmBtnBg = dangerMode
+        ? "bg-red-500 hover:bg-red-600"
+        : "bg-green-500 hover:bg-green-600";
+
+    const cancelBtnBg = dangerMode
+        ? "bg-green-500 hover:bg-green-600"
+        : "bg-red-500 hover:bg-red-600";
+
+    const modalHeaderColor = dangerMode ? "text-red-500" : "text-green-500";
+
+    return (
+        <div
+            className={`fixed top-6 right-0 left-0 flex items-center justify-center shadow-custom z-30 ${
+                open ? "block" : "hidden"
+            }`}
+        >
+            <div className="bg-white dark:bg-gray-800 rounded-xl shadow-xl max-w-sm w-full p-6 space-y-4">
+                <h3 className={`text-lg font-semibold ${modalHeaderColor}`}>{title}</h3>
+                <p className="text-gray-700 dark:text-gray-300">{message}</p>
+                <div className="flex justify-end gap-3">
+                    <button
+                        onClick={() => {
+                            setOpen(false);
+                            onCancel?.();
+                        }}
+                        className={`cursor-pointer px-3 py-1 rounded-lg text-white ${cancelBtnBg} transition`}
+                    >
+                        لغو
+                    </button>
+                    <button
+                        onClick={() => {
+                            setOpen(false);
+                            onConfirm?.();
+                        }}
+                        className={`cursor-pointer px-3 py-1 rounded-lg text-white ${confirmBtnBg} transition`}
+                    >
+                        تأیید
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/layout/TopBar/components/Left/AccountMenu/DropDownAccount.jsx
+++ src/layout/TopBar/components/Left/AccountMenu/DropDownAccount.jsx
@@ -1,20 +1,35 @@
 import {Link} from "react-router-dom";
 import {cn} from "@/lib/utils/ui-utils";
-import AccountAvatar from "../../common/AccountAvatar";
 import {useAuth} from "@context/AuthContext"
+import ConfirmModal from "@components/ui/ConfirmModal"
+import AccountAvatar from "../../common/AccountAvatar";
+import {logout} from "@api/callApi.js";
 
 function DropDownAccountOptions({data}) {
+    const iconElem = (
+        <span>
+            <svg className={"size-5"}>
+                <use href={`#${data.icon}-icon`}></use>
+            </svg>
+        </span>
+    )
+
     return (
         <ul>
             {data.map(link => {
                 return (
-                    <li>
-                        <span>
-                            <svg className={"size-5"}>
-                                <use href={`#${data.icon}-icon`}></use>
-                            </svg>
-                        </span>
-                        <Link to={`${data.url}`}>{data.name}</Link>
+                    <li className={"w-full"}>
+                        {link.url ? (
+                            <Link to={`${data.url}`} className={"w-full flex flex-row items-center gap-4"}>
+                                {iconElem}
+                                {data.name}
+                            </Link>
+                        ) : (
+                            <p {...link.props}>
+                                {iconElem}
+                                {data.name}
+                            </p>
+                        )}
                     </li>
                 )
             })}
@@ -40,11 +55,26 @@ function AccountInfo() {
 }
 
 export default function DropDownAccount({open, className}) {
+    // drop down options
+    const logoutHandler = () => {
+
+    }
+
+    const dropDownAccountOptionsData = [
+        {icon: "user", url: "/account", name: ""},
+        {icon: "logout", props: {onClick: logoutHandler}},
+    ];
+
     return (
-        <div className={cn("w-58 absolute top-full left-0 z-30 bg-main-bg py-2  rounded-md shadow-2xl", className, open ? "block" : "hidden")}>
+        <div className={cn("w-58 mt-2 px-4 absolute top-full left-0 z-30 bg-main-bg py-2  rounded-md shadow-2xl", className, open ? "block" : "hidden")}>
 
             {/* account info */}
             <AccountInfo/>
+
+            {/* logout in drop-down account menu */}
+            <div>
+
+            </div>
         </div>
     )
 }
