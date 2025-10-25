e09024a1fd15ebf363537188c925d598446e161e
homow <homowsh897@gmail.com>
2025-10-25 10:34:04 +0330

    config Loading Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import AppRoutes from "@/routes/AppRoutes.jsx";
-import {AuthProvider, useAuth} from "@/context/AuthContext.jsx"
+import {AuthProvider, useAuth} from "@/context/AuthContext"
+import Loading from "@components/ui/Loading";
 
 function InnerApp() {
     const {loading} = useAuth()
 
     return (
         loading ? (
-            <div className="fixed inset-0 size-full z-50 flex items-center justify-center min-h-screen bg-primary-bg">
-                <div className="flex flex-col items-center">
-                    <div className="size-20 border-8 border-t-violet-500 border-gray-300 rounded-full animate-spin"></div>
-                    <p className="mt-4 text-lg font-medium animate-pulse">یکم صبر کن ...</p>
-                </div>
-            </div>
+            <Loading/>
         ) : (
             <AppRoutes/>
         )
--- src/components/ui/SuspenseBoundary.jsx
+++ src/components/ui/SuspenseBoundary.jsx
@@ -1,7 +1,7 @@
 import {Suspense} from 'react'
 
 function SuspenseCallback() {
-    return <p className="font-medium text-secondary-txt text-xl text-center mt-4 p-4">در حال بارگذاری...</p>
+    return <p className="font-medium text-secondary-txt text-2xl text-center mt-10 p-4">در حال بارگذاری...</p>
 }
 
 export default function SuspenseBoundary({children, fallback}) {
