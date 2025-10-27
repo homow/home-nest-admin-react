974811cc22ce3f14ef0ee28fc36eeafe7928d7c4
homow <homowsh897@gmail.com>
2025-10-27 17:08:13 +0330

    update login form and logic

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -50,36 +50,54 @@ export default function Login() {
         };
 
         try {
+            // reset modal
             setAlertModalData({type: "error", message: ""});
             setIsOpenAlertModal(false);
 
             const res = await login(userInfo);
 
-            if (res.ok && res.user.role === "admin") {
-                setAlertModalData({type: "success", message: "خب، بالاخره وارد شدی."});
-                setIsOpenAlertModal(true);
+            if (res.ok) {
+                // admin
+                if (res.user.role === "admin") {
+                    setAlertModalData({type: "success", message: "خب، بالاخره وارد شدی."});
+                    setIsOpenAlertModal(true);
 
-                setTimeout(() => {
-                    setAuthInfo({userData: res.user, token: res.accessToken});
-                }, 3000)
+                    setTimeout(() => {
+                        setAuthInfo({userData: res.user, token: res.accessToken});
+                    }, 3000)
+                    // user
+                } else {
+                    setAlertModalData({type: "error", message: "اجازه ورود به این بخش رو نداری."})
+                }
+                setIsOpenAlertModal(true)
             }
 
         } catch (err) {
-            console.log(err);
-
             const data = err.response?.data || {};
             const status = err.response?.status;
+            let message;
 
-            let message = "خطای ناشناخته یا شبکه رخ داد";
-
-            if (status === 503 || data.error === "NETWORK_ERROR") {
-                message = "ارتباط با سرور برقرار نشد، اینترنت یا سرور را بررسی کن";
-            } else if (status === 401 || data.error === "INVALID_CREDENTIALS") {
-                message = "رمز یا ایمیلت اشتباهه";
-            } else if (status === 403 || data.error === "ACCESS_DENIED") {
-                message = "شما اجازه ورود ندارید";
-            } else if (status >= 500) {
-                message = "خطا از سمت سرور، بعداً تلاش کن";
+            switch (true) {
+                case (status === 503 || data.error === "NETWORK_ERROR"): {
+                    message = "ارتباط با سرور برقرار نشد، اینترنت یا سرور را بررسی کن";
+                    break;
+                }
+                case (status === 401 || data.error === "INVALID_CREDENTIALS"): {
+                    message = "رمز یا ایمیلت اشتباهه";
+                    break;
+                }
+                case (status === 403 || data.error === "ACCESS_DENIED"): {
+                    message = "شما اجازه ورود ندارید";
+                    break;
+                }
+                case (status >= 500): {
+                    message = "خطا از سمت سرور، بعداً تلاش کن";
+                    break;
+                }
+                default: {
+                    message = "مشکلی ناشناخته";
+                    break;
+                }
             }
 
             setAlertModalData({
