4f4fe5ee62fedbee214862976ee3a80a046b807c
homow <homowsh897@gmail.com>
2025-11-03 21:12:14 +0330

    update imports in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import BASE_PATH from "@/config.js";
-import "@/styles/global.css";
+import "./styles/global.css";
 import App from './App';
 
 createRoot(document.getElementById('root')).render(
