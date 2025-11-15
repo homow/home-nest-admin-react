524e228a734649f7a5df9c0e57c2ff9b70c58756
homow <homowsh897@gmail.com>
2025-11-15 06:13:49 +0330

    config helper for CollapsedContext

--- src/lib/utils/ui-utils.js
+++ src/lib/utils/ui-utils.js
@@ -3,4 +3,17 @@ import {twMerge} from "tailwind-merge";
 
 const cn = (...inputs) => twMerge(clsx(inputs));
 
+const applyCustomSpace = ({setCurrentCollapsed, collapsed}) => {
+    if (window.innerWidth < 900) {
+        document.documentElement.style.setProperty("--spacing-custom", "0px");
+        setCurrentCollapsed(false);
+    } else {
+        document.documentElement.style.setProperty(
+            "--spacing-custom",
+            collapsed ? "80px" : "256px"
+        );
+        setCurrentCollapsed(collapsed);
+    }
+}
+
 export {cn};
