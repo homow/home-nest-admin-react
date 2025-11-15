2fe91acce78fb76ef81050762c80253156adc3c1
homow <homowsh897@gmail.com>
2025-11-15 07:36:31 +0330

    fix bug when load login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import {Outlet} from "react-router-dom";
 import {AuthProvider, useAuth} from "@context/AuthContext"
 import Loading from "@components/ui/Loading";
-import MainLayout from "@/layout/MainLayout/index";
 
 function InnerApp() {
     const {loading} = useAuth()
 
     return (
         loading ? (
-            <Loading/>
+            <Loading className={"fixed inset-0"}/>
         ) : (
-            <MainLayout>
-                <Outlet/>
-            </MainLayout>
+            <Outlet/>
         )
     )
 }
