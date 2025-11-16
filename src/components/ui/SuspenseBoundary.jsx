68f98c3d584a7ca2252a17cb8863856c99b58814
homow <homowsh897@gmail.com>
2025-11-16 06:42:02 +0330

    refactor login routes with lazy suspense

--- src/components/ui/SuspenseBoundary.jsx
+++ src/components/ui/SuspenseBoundary.jsx
@@ -1,9 +1,9 @@
 import {Suspense} from 'react'
 import Loading from "./Loading";
 
-export default function SuspenseBoundary({children, fallback}) {
+export default function SuspenseBoundary({children, fallback, className}) {
     return (
-        <Suspense fallback={fallback || <Loading msg={"در حال بارگذاری"}/>}>
+        <Suspense fallback={fallback || <Loading className={className} msg={"در حال بارگذاری"}/>}>
             {children}
         </Suspense>
     )
