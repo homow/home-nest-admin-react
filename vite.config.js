25421e89116930a85ba4b7a9f1fa00c027caf43c
homow <homowsh897@gmail.com>
2025-11-29 22:24:55 +0330

    update configs

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import {defineConfig, loadEnv} from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite";
 import path from 'path';
 import {fileURLToPath} from "url";
@@ -61,7 +61,13 @@ export default defineConfig(({mode}) => {
             }
         },
         plugins: [
-            react(),
+            react(
+                {
+                    babel: {
+                        plugins: ["babel-plugin-react-compiler"]
+                    }
+                }
+            ),
             tailwindcss(),
             env.ANALYZE === "true" &&
             visualizer({
