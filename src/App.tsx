28cf58ef1c5cd53dcc25c6a3d8a0e43d947d5119
homow <homowsh897@gmail.com>
2025-12-14 11:32:31 +0330

    change file form js to ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {Outlet} from "react-router-dom";
+import {Outlet} from "react-router";
 import {AuthProvider, useAuth} from "@context/AuthContext"
 import Loading from "@components/ui/Loading";
 
