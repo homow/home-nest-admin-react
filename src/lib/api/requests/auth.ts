13dd8322227183261c580f23066985a142816103
homow <homowsh897@gmail.com>
2025-12-18 19:21:56 +0330

    add types to js file

--- src/lib/api/requests/auth.ts
+++ src/lib/api/requests/auth.ts
@@ -1,26 +1,25 @@
-"use strict";
-
+import type {UserLogin} from "@/types/auth.types";
 import axios from "axios";
-import {API_URL} from "@/config.ts";
+import {API_URL} from "@/config";
 
 const BASE_API_URL = `${API_URL}`;
 
-const login = async (userInfo) => {
+async function login(userInfo: UserLogin) {
     const res = await axios.post(`${BASE_API_URL}/api/auth/login`, {...userInfo});
     const {ok, user, accessToken} = res.data;
 
     if (!ok) {
-        const err = new Error("LOGIN_FAILED");
-        err.payload = res.data;
-        throw err;
+        throw new Error(res.data.message || "LOGIN_FAILED");
     }
 
     return {ok: true, user, accessToken};
-};
+}
 
-const refresh = async () => {
+async function refresh() {
     try {
-        const res = await axios.post(`${BASE_API_URL}/api/auth/refresh`, {}, {withCredentials: true});
+        const res = await axios.post(`${BASE_API_URL}/api/auth/refresh`, {}, {
+            withCredentials: true
+        });
 
         if (res.data.ok) {
             return res.data;
@@ -28,16 +27,18 @@ const refresh = async () => {
     } catch (e) {
         return {ok: false, error: e};
     }
-};
+}
 
-const logout = async () => {
+async function logout() {
     try {
-        const res = await axios.post(`${BASE_API_URL}/api/auth/logout`, {}, {withCredentials: true});
+        const res = await axios.post(`${BASE_API_URL}/api/auth/logout`, {}, {
+            withCredentials: true
+        });
 
         return {ok: true, res}
     } catch (e) {
         return {ok: false, error: e};
     }
-};
+}
 
 export {login, refresh, logout};
